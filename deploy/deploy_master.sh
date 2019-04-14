b6c38cf5da3b9acc1dd5710b3a2adf169e104282
root <root@umair.space>
2019-04-14 08:34:06 -0400

    circleci config

--- deploy/deploy_master.sh
+++ deploy/deploy_master.sh
@@ -12,7 +12,7 @@ ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker rmi fan0o/dockerdocker'
 ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker login -u ' $DOCKER_USER ' -p ' $DOCKER_PASS ''
 
 echo "pulling latest version of the code"
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker pull fan0o/dockerdocker:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker pull fan0o/dockerdocker:flask'
 
 echo "starting the new live version"
 ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'docker  run -d -p 4000:80 --restart=always --name dockerdocker fan0o/dockerdocker'
