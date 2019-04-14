32b2431b65c4946c6533ecb2d64c26722b68013b
root <root@umair.space>
2019-04-14 09:32:22 -0400

    adding prod in scripts

--- deploy/deploy_prod.sh
+++ deploy/deploy_prod.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Creating Logs"
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker logs dockerdocker > /home/ubuntu/logs-archive/dockerdocker-dev_`date +%Y%m%d-%H%M%S`.log'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker logs dockerdocker > /root/logs-archive/dockerdocker-prod_`date +%Y%m%d-%H%M%S`.log'
 
 echo "stopping running live application"
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker stop dockerdocker'
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker rm dockerdocker'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker stop dockerdocker:prod'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker rm dockerdocker:prod'
 
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker rmi fan0o/dockerdocker:flask'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker rmi fan0o/dockerdocker:prod'
 
 ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker login -u ' $DOCKER_USER ' -p ' $DOCKER_PASS ''
 
 echo "pulling latest version of the code"
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker pull fan0o/dockerdocker:flask'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'sudo docker pull fan0o/dockerdocker:prod'
 
 echo "starting the new live version"
-ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'docker  run -d -p 4000:80 --restart=always --name dockerdocker fan0o/dockerdocker:flask'
+ssh $DEPLOY_USER@$DEPLOY_HOST_MASTER 'docker  run -d -p 4000:80 --restart=always --name dockerdocker fan0o/dockerdocker:prod'
 
 echo "success!"
 
--- deploy/registry_prod.sh
+++ deploy/registry_prod.sh
@@ -4,10 +4,10 @@ echo "${DOCKER_PASS}" > DOCKER_PASS
 cat DOCKER_PASS | docker login --username $DOCKER_USER --password-stdin
 
 echo "Docker Tagging"
-docker tag fan0o/dockerdocker fan0o/dockerdocker:flask
+docker tag fan0o/dockerdocker fan0o/dockerdocker:prod
 
 echo "Docker push to registry"
-docker push fan0o/dockerdocker:flask
+docker push fan0o/dockerdocker:prod
 
 echo "success!"
 
