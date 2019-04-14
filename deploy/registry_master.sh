420af1e4789ed744f250d9f593781ff56b6f0f80
root <root@umair.space>
2019-04-14 07:46:04 -0400

    circleci config

--- deploy/registry_master.sh
+++ deploy/registry_master.sh
@@ -4,7 +4,7 @@ echo "${DOCKER_PASS}" > DOCKER_PASS
 cat DOCKER_PASS | docker login --username $DOCKER_USER --password-stdin
 
 echo "Docker Tagging"
-docker tag imagebuildinginprocess fan0o/dockerdocker:flask
+docker tag dockerdocker fan0o/dockerdocker:flask
 
 echo "Docker push to registry"
 docker push fan0o/dockerdocker:flask
